d4619cc502d13a4472c51215630caf5dfddb3018
TELEX2009 <sssridhar2009@gmail.com>
2025-04-19 14:54:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Blackpearl", "🎮 Playing With Blackpearl Pirates"];
+const statusMessages = ["🎧 Listening to Owner Kutty Advices", "🎮 Playing With Blackpearl Pirates"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
